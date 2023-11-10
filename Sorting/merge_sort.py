e4b41625d8af645f350317d6d14c4ba5f34a1b8c
Reyad <69958555+MdReyadHossain@users.noreply.github.com>
2023-11-11 05:27:04 +0600

    merge sort created

---
+++ Sorting/merge_sort.py
@@ -0,0 +1,12 @@
+from list import List
+
+
+class BubbleSort(List):
+    def __init__(self, list: list):
+        super().__init__(list, len(list), 'Merge Sort')
+
+    def ascendingSort(self):
+        print()
+
+    def descendingSort(self):
+        print()
