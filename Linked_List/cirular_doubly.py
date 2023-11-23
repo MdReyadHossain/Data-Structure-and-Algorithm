8f00e6baa1fe89c1f31c5761ee41cfc212301dea
reyad-orangetoolz <reyadhossain@orangetoolz.com>
2023-11-23 19:02:15 +0600

    circular doubly list

--- Linked_List/cirular_doubly.py
+++ Linked_List/cirular_doubly.py
@@ -5,4 +5,22 @@ from node import Node
 class CircularDoublyList(List):
     def __init__(self):
         super().__init__(None, 0)
-        self.tail = None
+        self.tail: Node = None
+
+    def insertAtFirst(self, data):
+        newNode: Node = Node(data)
+        if self.size == 0:
+            self.head = newNode
+            newNode.next = self.head
+            self.tail = newNode
+            self.tail.next = self.head
+        else:
+            self.head.previous = newNode
+            newNode.next = self.head
+            self.head = newNode
+            self.tail.next = self.head
+
+    def printList(self):
+        currentPtr: Node = self.head
+        while currentPtr.next != self.head:
+            print()
